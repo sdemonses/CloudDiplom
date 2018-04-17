83102ed86d978d40851ad4fe545db728e00b5e93
Dmytro <sdemonses@gmail.com>
2018-04-18 01:11:46 +0300

    Security changes

--- Authorization/src/main/kotlin/ua/nure/authorization/AuthorizationApplication.kt
+++ Authorization/src/main/kotlin/ua/nure/authorization/AuthorizationApplication.kt
@@ -1,27 +1,60 @@
 package ua.nure.authorization
 
+import org.apache.catalina.filters.RequestDumperFilter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.security.Principal
+import org.springframework.web.bind.annotation.SessionAttributes
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 
 @SpringBootApplication
 @EnableAuthorizationServer
-@EnableDiscoveryClient
-@EnableResourceServer
+@EnableEurekaClient
 @RestController
+@SessionAttributes("authorizationRequest")
 class AuthorizationApplication {
 
-    @RequestMapping("/user")
-    fun user(user: Principal): Principal {
-        return user
+    @Configuration
+    internal class MvcConfig : WebMvcConfigurerAdapter() {
+        override fun addViewControllers(registry: ViewControllerRegistry?) {
+            registry!!.addViewController("login").setViewName("login")
+            registry.addViewController("/oauth/confirm_access").setViewName("authorize")
+            registry.addViewController("/").setViewName("index")
+        }
+    }
+
+    @Configuration
+    internal class LoginConfig : WebSecurityConfigurerAdapter() {
+        @Throws(Exception::class)
+        override fun configure(http: HttpSecurity) {
+            http
+                    .formLogin().loginPage("/login").permitAll()
+                    .and()
+                        .requestMatchers()
+                        .antMatchers("/", "/login", "/oauth/authorize", "/oauth/confirm_access")
+                    .and()
+                        .authorizeRequests()
+                        .anyRequest().authenticated()
+
+        }
+    }
+
+    @Profile("!cloud")
+    @Bean
+    internal fun requestDumperFilter(): RequestDumperFilter {
+        return RequestDumperFilter()
     }
 }
+
 fun main(args: Array<String>) {
     runApplication<AuthorizationApplication>(*args)
 }
--- Gateway/src/main/kotlin/ua/nure/gateway/GatewayApplication.kt
+++ Gateway/src/main/kotlin/ua/nure/gateway/GatewayApplication.kt
@@ -1,14 +1,40 @@
 package ua.nure.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.context.annotation.Bean
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.security.oauth2.client.token.AccessTokenProvider
+import org.springframework.security.oauth2.client.token.AccessTokenProviderChain
+import org.springframework.security.oauth2.client.token.OAuth2AccessTokenSupport
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider
+import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider
+import java.util.stream.Collectors
+
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
-class GatewayApplication
+@EnableEurekaClient
+class GatewayApplication {
+    @Bean
+    fun userInfoRestTemplateCustomizer(loadBalancerInterceptor: LoadBalancerInterceptor): UserInfoRestTemplateCustomizer {
+        return UserInfoRestTemplateCustomizer {
+            val interceptors = mutableListOf<ClientHttpRequestInterceptor>()
+            interceptors.add(loadBalancerInterceptor)
+            val accessTokenProviderChain = AccessTokenProviderChain(listOf<OAuth2AccessTokenSupport>(AuthorizationCodeAccessTokenProvider(), ImplicitAccessTokenProvider(),
+                    ResourceOwnerPasswordAccessTokenProvider(), ClientCredentialsAccessTokenProvider()).stream()
+                    .peek { it.setInterceptors(interceptors) }
+                    .collect(Collectors.toList()) as MutableList<out AccessTokenProvider>?)
+            it.setAccessTokenProvider(accessTokenProviderChain)
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
