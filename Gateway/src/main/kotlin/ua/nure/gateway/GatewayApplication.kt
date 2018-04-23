7823fc4a5a38740c8403c9343b73d7c5311953ff
Dmytro <sdemonses@gmail.com>
2018-04-23 23:53:13 +0300

    Gradle changes

--- Gateway/src/main/kotlin/ua/nure/gateway/GatewayApplication.kt
+++ Gateway/src/main/kotlin/ua/nure/gateway/GatewayApplication.kt
@@ -1,22 +1,9 @@
 package ua.nure.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
-import org.springframework.context.annotation.Bean
-import org.springframework.http.client.ClientHttpRequestInterceptor
-import org.springframework.security.oauth2.client.token.AccessTokenProvider
-import org.springframework.security.oauth2.client.token.AccessTokenProviderChain
-import org.springframework.security.oauth2.client.token.OAuth2AccessTokenSupport
-import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider
-import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider
-import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider
-import java.util.stream.Collectors
 
 
 @SpringBootApplication
