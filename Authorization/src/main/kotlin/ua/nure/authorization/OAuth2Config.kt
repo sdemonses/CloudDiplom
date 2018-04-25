d9e647fde0513e313e2c9c50dacdf80bffd41977
Dmytro Biblyi <dmytro_biblyi@epam.com>
2018-04-25 16:42:34 +0300

    Authorization server

--- Authorization/src/main/kotlin/ua/nure/authorization/OAuth2Config.kt
+++ Authorization/src/main/kotlin/ua/nure/authorization/OAuth2Config.kt
@@ -1,24 +1,20 @@
 package ua.nure.authorization
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore
-import javax.sql.DataSource
 
 @EnableAuthorizationServer
 @Configuration
-class OAuth2Config(val authenticationManager: AuthenticationManager, val dataSource: DataSource) : AuthorizationServerConfigurerAdapter() {
+class OAuth2Config(val authenticationManager: AuthenticationManager) : AuthorizationServerConfigurerAdapter() {
 
-    override fun configure(endpoints: AuthorizationServerEndpointsConfigurer) {
+    /*override fun configure(endpoints: AuthorizationServerEndpointsConfigurer) {
         endpoints
                 .authenticationManager(this.authenticationManager)
                 .tokenStore(tokenStore())
-    }
+    }*/
 
     override fun configure(oauthServer: AuthorizationServerSecurityConfigurer) {
         oauthServer
@@ -26,8 +22,8 @@ class OAuth2Config(val authenticationManager: AuthenticationManager, val dataSou
                 .checkTokenAccess("isAuthenticated()")
     }
 
-    @Bean
-    fun tokenStore(): JdbcTokenStore {
-        return JdbcTokenStore(dataSource)
-    }
+    /*  @Bean
+      fun tokenStore(): JdbcTokenStore {
+          return JdbcTokenStore(dataSource)
+      }*/
 }
--- Gateway/src/main/kotlin/ua/nure/gateway/GatewayApplication.kt
+++ Gateway/src/main/kotlin/ua/nure/gateway/GatewayApplication.kt
@@ -1,15 +1,95 @@
 package ua.nure.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.oauth2.client.OAuth2RestTemplate
+import org.springframework.security.web.csrf.CsrfToken
+import org.springframework.security.web.csrf.CsrfTokenRepository
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository
+import org.springframework.security.web.session.SessionManagementFilter
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.util.WebUtils
+import java.util.*
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
-class GatewayApplication
+@EnableEurekaClient
+@EnableOAuth2Sso
+class GatewayApplication {
+
+    @Configuration
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    protected class SecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+
+        @Throws(Exception::class)
+        override fun configure(http: HttpSecurity) {
+            http
+                    .authorizeRequests()
+                    //Allow access to all static resources without authentication
+                    .antMatchers("/", "/**/*.html").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .csrf().csrfTokenRepository(csrfTokenRepository())
+                    .and()
+                    .addFilterAfter(csrfHeaderFilter(), SessionManagementFilter::class.java)
+
+            //http.httpBasic().disable();
+        }
+
+        private fun csrfHeaderFilter(): Filter {
+            return object : OncePerRequestFilter() {
+                override fun doFilterInternal(request: HttpServletRequest,
+                                              response: HttpServletResponse, filterChain: FilterChain) {
+                    val csrf = request.getAttribute(CsrfToken::class.java.name) as CsrfToken
+                    if (csrf != null) {
+                        var cookie: Cookie? = WebUtils.getCookie(request, "XSRF-TOKEN")
+                        val token = csrf.token
+                        if (cookie == null || token != null && token != cookie.value) {
+                            cookie = Cookie("XSRF-TOKEN", token)
+                            cookie.path = "/"
+                            response.addCookie(cookie)
+                        }
+                    }
+                    filterChain.doFilter(request, response)
+                }
+            }
+        }
+
+        private fun csrfTokenRepository(): CsrfTokenRepository {
+            val repository = HttpSessionCsrfTokenRepository()
+            repository.setHeaderName("X-XSRF-TOKEN")
+            return repository
+        }
+    }
+}
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+internal class WorkaroundRestTemplateCustomizer : UserInfoRestTemplateCustomizer {
+
+    override fun customize(template: OAuth2RestTemplate) {
+        template.interceptors = ArrayList(template.interceptors)
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
